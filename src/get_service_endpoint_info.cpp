be3986f45d444c6affab4e39a2fdf1f31014e65c
matlin <davide.cst00@gmail.com>
2026-07-02 14:13:34 +0200

    Changed CBorStream abstraction paradigm

--- src/get_service_endpoint_info.cpp
+++ src/get_service_endpoint_info.cpp
@@ -25,4 +25,3 @@ rmw_ret_t rmw_get_servers_info_by_service(const rmw_node_t * node, rcutils_alloc
   RMW_SET_ERROR_MSG("rmw_get_servers_info_by_service not implemented");
   return RMW_RET_UNSUPPORTED;
 }
-
