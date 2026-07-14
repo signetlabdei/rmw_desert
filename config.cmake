becc7d0fefeb4500a7668286c06ee3cfe5e1febe
Dmytro Ochkas <dmytro.ochkas@imt-atlantique.fr>
2026-07-14 11:44:20 +0200

    Integrate ascon in Security Layer and add submodules

--- config.cmake
+++ config.cmake
@@ -1,6 +1,6 @@
 set(CBOR_LIB "NanoCBOR" CACHE STRING "Cbor library.") # libmcu_cbor, NanoCBOR
 option(SECURE_MODE_ENABLED "" ON)
-set(CRYPTO_LIB "mbedtls" CACHE STRING "Crypto library.")
+set(CRYPTO_LIB "ascon-c" CACHE STRING "Crypto library.")
 set(AEAD_ALGO "Ascon-AEAD128" CACHE STRING "Algorithm for authenticated encryption with additional data.")
 set(KDF_ALGO "HKDF-Ascon256" CACHE STRING "Algorithm for key derivation.")
 set(SENDER_ID "0x01" CACHE STRING "Sender ID in hex.")
